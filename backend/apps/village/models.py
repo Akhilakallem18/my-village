e8c19554fe892f027d721337bc68d915edecc52b
Akhila Kallem <akhilakallem@Mac.lan>
2026-07-29 13:59:01 -0500

    Add Django backend foundation and village data model

---
+++ backend/apps/village/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Village(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    known_for = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
